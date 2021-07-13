2dac86c1deb1313f3348f6d521c425ff67f998a1
praveensumathi98 <praveensumathi98@gmail.com>
2021-07-13 16:03:02 +0530

    Allow Cors

--- Session_Feedback/Startup.cs
+++ Session_Feedback/Startup.cs
@@ -31,6 +31,7 @@ namespace Session_Feedback
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllersWithViews()
                 .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
@@ -72,6 +73,14 @@ namespace Session_Feedback
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
